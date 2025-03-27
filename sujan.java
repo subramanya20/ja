2843df226c655d6b211b9b32f2db68a9d675b66b
subramanya20 <83331261+subramanya20@users.noreply.github.com>
2025-03-27 11:51:32 +0530

    Create sujan.java

---
+++ sujan.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+        scanner.close();
+    }
+}
